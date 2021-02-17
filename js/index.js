8fefea9ff252ec2020f8fdf72e08b194d4af8ec0
juune622 <juune622@naver.com>
2021-02-17 23:34:37 +0900

    ...

--- js/index.js
+++ js/index.js
@@ -2,11 +2,22 @@
 	 el: '#app',
 	 data: {
 		 title:'',
-		 boxShow:false
+		 boxShow:false,
+		 imgArr:[
+			'../img/iu1.jpg',
+			'../img/iu2.jpg',
+			'../img/iu3.jpg'
+		 ],
+		 imgSrc: '../img/iu1.jpg',
+		 imgIdx: 0,
 	 },
 	 methods: {
 		onBtnClick(){
 			this.boxShow=!this.boxShow; 
+		},
+		onImgClick(){
+			this.imgIdx = (this.imgArr.length - 1 == this.imgIdx) ? 0 : this.imgIdx + 1;
+			this.imgSrc = this.imgArr[this.imgIdx];
 		}
 	 }
  });
